a7a4aacc1bd0a0ed4b5305149b7457dea392ef63
Daniel Thompson <daniel@redfelineninja.org.uk>
2014-01-20 08:40:03 +0000

    Provide an example demonstrating automatic source code discovery.
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

---
+++ examples/libzero.cpp
@@ -0,0 +1,26 @@
+/*
+ * libzero.cpp
+ *
+ * Part of hbcxx - executable C++ source code
+ *
+ * Copyright (C) 2014 Daniel Thompson <daniel@redfelineninja.org.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+/*!
+ * \file libzero.cpp
+ *
+ * This file will be automatically discovered when the include example 
+ * is executed. See the comments in include for more details.
+ */
+
+#include "libzero.h"
+
+int zero::zero()
+{
+	return 0;
+}
