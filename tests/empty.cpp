d2ebc52a22bce7a11bc5d3b6385dbcffde8e5a0e
Daniel Thompson <daniel@redfelineninja.org.uk>
2014-10-29 21:57:48 +0000

    tests: empty and flags tests
    Signed-off-by: Daniel Thompson <daniel@redfelineninja.org.uk>

---
+++ tests/empty.cpp
@@ -0,0 +1,28 @@
+#!/usr/bin/env hbcxx
+
+/*
+ * empty.cpp
+ *
+ * Part of hbcxx - executable C++ source code
+ *
+ * Copyright (C) 2014 Daniel Thompson <daniel@redfelineninja.org.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+/*!
+ * \file empty.cpp
+ *
+ * The "empty" test is the simplest valid C++ program.
+ *
+ * It is run both as a test case in its own right and also included in
+ * other tests to check source discovery features.
+ */
+
+int main()
+{
+    return 0;
+}
---
+++ tests/flags.cpp
@@ -0,0 +1,28 @@
+#!/usr/bin/env hbcxx
+
+/*
+ * flags.cpp
+ *
+ * Part of hbcxx - executable C++ source code
+ *
+ * Copyright (C) 2014 Daniel Thompson <daniel@redfelineninja.org.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+/*!
+ * \file flags.cpp
+ *
+ * Test that flags are picked up and applied by the compiler.
+ */
+
+//#! -DFLAGS_TEST=int
+typedef FLAGS_TEST flags_test_t;
+
+int main()
+{
+    return 0;
+}
